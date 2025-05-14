d9b70136854e200fd18fbfaf65d3cd66adef360c
Annika Gadman <annika.gadman@gmail.com>
2025-05-13 22:44:29 -0500

    rensat upp och lagt in routes samt httpClient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "Home", component: HomeComponent },
+    { path: "Courses", component: CoursesComponent},
+    { path: "Schedule", component: ScheduleComponent },
+    { path: "", redirectTo: "Home", pathMatch: "full" }
+];
