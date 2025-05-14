fbf9dfc3df2b2e8697b50812dd211f7639064400
Annika Gadman <annika.gadman@gmail.com>
2025-05-14 10:42:27 -0500

    laddar in kurser från json

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Course } from '../models/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
+  courses = signal<Course[]>([]); 
+  error = signal<string | null>(null);
 
+  coursesService = inject(CoursesService);
+
+  ngOnInit() {
+    this.loadCourses();
+  }
+
+  async loadCourses() {
+    try {
+      const response = await this.coursesService.loadCourses();
+      this.courses.set(response);
+      console.table(this.courses());
+    } catch (error) {
+      console.error(error);
+      this.error.set("Kunde inte ladda kurser - försök igen senare.");
+    }
+  }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Course } from '../models/course';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
+  private url: string = "/miun_courses.json";
 
-  constructor() { }
+  http = inject(HttpClient);
+
+  // Laddar kurser
+  async loadCourses(): Promise<Course[]> {
+    const courses = this.http.get<Course[]>(this.url);
+    return await firstValueFrom(courses);
+  }
 }
