806c6b9b6c7a6c66ffcfc31b85469466deaf9744
Annika Gadman <annika.gadman@gmail.com>
2025-05-17 09:54:14 -0500

    implementerat navigering komponent och startat styling globalt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { CoursesComponent } from './courses/courses.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 
 export const routes: Routes = [
-    { path: "Home", component: HomeComponent },
-    { path: "Courses", component: CoursesComponent},
-    { path: "Schedule", component: ScheduleComponent },
-    { path: "", redirectTo: "Home", pathMatch: "full" }
+    { path: "home", component: HomeComponent },
+    { path: "courses", component: CoursesComponent},
+    { path: "schedule", component: ScheduleComponent },
+    { path: "", redirectTo: "home", pathMatch: "full" }
 ];
